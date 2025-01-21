01234e9272a53e36cd0059305e6f71d3f8e5e0b3
bernardosousa <bernardodiasousa@gmail.com>
2025-01-21 15:38:25 +0000

    Setup dependency injection

---
+++ app/src/main/java/com/example/bity/di/AppModule.kt
@@ -0,0 +1,47 @@
+package com.example.bity.di
+
+import com.example.bity.data.api.EtherscanApi
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object ApiModule {
+
+    private const val BASE_URL = "https://api.etherscan.io/"
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(): OkHttpClient {
+        val loggingInterceptor = HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+
+        return OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(okHttpClient)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideEtherscanApi(retrofit: Retrofit): EtherscanApi {
+        return retrofit.create(EtherscanApi::class.java)
+    }
+}
